e0949cf56dd5ffc79e8270b0e502cbd65390c029
bravemaster3 <noumonvikoffidodji@hotmail.fr>
2023-07-28 10:54:09 +0200

    Add Plotter daily avg
    This function is like plotter_daily, but instead of using a loess smoothing, it uses rolling average where the windows size can be specified.

---
+++ R/plotter_daily_avg.R
@@ -0,0 +1,93 @@
+#' Daily plotter for all sites for a given variable
+#'
+#' @param list_df a list of dataframes having the same structure and able to be bound by row. When this comes from the saved aggregated tables, they are just the _day element of the list.
+#' @param y_label label of the y axis. it can also be a bquote if superscripts or underscripts are needed.
+#' @param y_col string, name of the variable to be represented on the y axis.
+#' @param show_x_label logical, TRUE to show x-axis text and title, FALSE to hider x-axis text and title.
+#' @param ignore_NA logical. If TRUE, segments without NA will be plotted separately to avoid interpolation of geom_smooth where there are missing values
+#' @param GPP logical. If TRUE, the graph us then tailored for GPP. default is FALSE
+#' @param Reco logical. If TRUE, the graph us then tailored for Reco. default is FALSE
+#' @param NEE logical. If TRUE, the graph us then tailored for NEE. default is FALSE
+#' @param yintercept_line logical. If TRUE, a horizontal line will be added. default is FALSE
+#' @param yintercept_value positive integer. specifies the y value at which the horizonal line will be drawn. default is 0
+#' @param scale_x One of "month_only or "month_year", default is month_only
+#' @param rotate_x_label Logical. default, FALSE
+#' @param conf_int Logical. Display a ribbon confidence interval around the smoothed lines. default, TRUE.
+#' @param window_size window size for centered rolling average, default is 14 days
+#'
+#' @return a ggplot object
+#' @export
+
+plotter_daily_avg <- function (list_df, y_label, y_col = "FCH4_sum", show_x_label = FALSE,
+                               ignore_NA = TRUE, GPP = FALSE, Reco = FALSE, NEE = FALSE,
+                               yintercept_line = FALSE, yintercept_value = 0, scale_x = "month_only",
+                               rotate_x_label = FALSE, conf_int = TRUE, window_size = 14)
+{
+
+  for(i in 1:length(list_df))
+  {
+    list_df[[i]] <- list_df[[i]] %>%
+      dplyr::mutate(Var_smooth = zoo::rollmean(.data[[y_col]], k = window_size, fill=NA, align = "center"))
+  }
+  df <- Reduce(function(...) rbind(...), list_df)
+  g <- ggplot2::ggplot(data = df, ggplot2::aes_string(x = "date",
+                                                      y = y_col, color = "Site"))
+  if (isTRUE(yintercept_line)) {
+    g <- g + ggplot2::geom_hline(yintercept = yintercept_value,
+                                 color = "black", linewidth = 0.5, linetype = "dashed")
+  }
+  g <- g + ggplot2::geom_point(alpha = 0.1)
+  df2 <- df
+  if (GPP == TRUE) {
+    df2 <- df2 %>% dplyr::group_by(Site) %>% dplyr::mutate(GPP_sum = ifelse(is.na(GPP_sum) &
+                                                                              date > as.Date("2020-12-31"), zoo::na.approx(GPP_sum),
+                                                                            GPP_sum))
+  }
+  if (NEE == TRUE) {
+    df2 <- df2 %>% dplyr::group_by(Site) %>% dplyr::mutate(NEE_sum = ifelse(is.na(NEE_sum) &
+                                                                              date > as.Date("2020-12-31"), zoo::na.approx(NEE_sum),
+                                                                            NEE_sum))
+  }
+  if (Reco == TRUE) {
+    df2 <- df2 %>% dplyr::group_by(Site) %>% dplyr::mutate(Reco_sum = ifelse(is.na(Reco_sum) &
+                                                                               date > as.Date("2020-12-31"), zoo::na.approx(Reco_sum),
+                                                                             Reco_sum))
+  }
+  if (ignore_NA == TRUE) {
+    df2$na_y <- factor(ifelse(is.na(df2[, y_col]), 0, 1))
+    df2$bloc <- factor(data.table::rleid(df2$na_y))
+    for (i in unique(df2$bloc)) {
+      if (unique(df2[df2$bloc == i, "na_y"]) == 1) {
+        g <- g + ggplot2::geom_line(data = df2[df2$bloc ==
+                                                 i, ], aes(y = Var_smooth), alpha = 0.6, linewidth = 1.5)
+      }
+    }
+  } else {
+    g <- g + ggplot2::geom_line(data = df2, aes(y = Var_smooth), alpha = 0.6, linewidth = 1.5)
+  }
+  g <- g + ggplot2::ylab(y_label) + ggplot2::theme_classic() +
+    ggplot2::theme(panel.border = ggplot2::element_rect(fill = NA,
+                                                        size = 1)) + ggplot2::theme(legend.position = c(0.85,
+                                                                                                        0.8), legend.background = ggplot2::element_rect(fill = "white",
+                                                                                                                                                        colour = "black", size = 0.5, linetype = 2), legend.text = ggplot2::element_text(size = 8),
+                                                                                    legend.title = ggplot2::element_blank(), legend.spacing.y = ggplot2::unit(0.01,
+                                                                                                                                                              "cm"))
+  if (scale_x == "month_year") {
+    g <- g + ggplot2::scale_x_date(breaks = "1 month", labels = scales::date_format("%b/%Y"))
+  }
+  if (scale_x == "month_only") {
+    g <- g + ggplot2::scale_x_date(date_breaks = "1 month",
+                                   labels = dte_formatter, expand = c(0, 0))
+  }
+  if (isTRUE(show_x_label)) {
+    if (isTRUE(rotate_x_label)) {
+      g <- g + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90))
+    }
+  }
+  if (isFALSE(show_x_label)) {
+    g <- g + ggplot2::theme(axis.text.x = ggplot2::element_blank(),
+                            axis.title.x = ggplot2::element_blank())
+  }
+  g
+}
+
