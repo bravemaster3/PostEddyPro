a419f22337b4346b87d85db515082fe9c8770284
bravemaster3 <noumonvikoffidodji@hotmail.fr>
2024-03-28 14:17:07 +0100

    Changed reddyproc_gapfiller
    The old one is now renamed old,
    The new one is using ustar filtering, and running year by year.
    It seems like the Rpackage doesn't work properly when processing multiple years at once.
    
    Also, this function EddyProc.C$sMRFluxPartitionUStarScens() currently used gets closer to the online version of ReddyProc, compared to EddyProc.C$sMRFluxPartition() that I was using before, which led to very little vertical fluctuation before.

--- R/reddyproc_gapfiller.R
+++ R/reddyproc_gapfiller.R
@@ -1,4 +1,4 @@
-#' A wrapper for ReddyProc gapfiller for gapfilling NEE or H2O fluxes,and partitionning NEE
+#' A wrapper for ReddyProc gapfiller for gapfilling NEE or H2O fluxes,and partitionning NEE. Each year is gapfilled/partitioned separately and merged
 #'
 #' @param formatted_file_path path to the file ready for ReddyProc gapfilling. Typically the output from the "formatting_fluxes_REddyProc" function of the PostEddyPro package
 #' @param saving_folder path to the folder where the gapfilled fluxes will be saved
@@ -20,44 +20,79 @@ reddyproc_gapfiller <- function(formatted_file_path,
                                 timezone=1,
                                 gapfill_flux=TRUE){
   #i=1 #turn on for testing
-  EddyData.F <- REddyProc::fLoadTXTIntoDataframe(formatted_file_path)
+  all_df <- REddyProc::fLoadTXTIntoDataframe(formatted_file_path)
 
-  #+++ If not provided, calculate VPD from Tair and rH
-  # EddyData.F <- cbind(EddyData.F,VPD=fCalcVPDfromRHandTair(EddyData.F$rH, EddyData.F$Tair))
+  year_index = 0
+  for (year in unique(all_df$Year)){
+    message(paste("Now gapfilling/partitionning Year:", year))
+    year_index = year_index+1
+    EddyData.F <- all_df %>%
+      dplyr::filter(Year == year)
 
-  #+++ Add time stamp in POSIX time format
-  EddyDataWithPosix.F <- REddyProc::fConvertTimeToPosix(EddyData.F, 'YDH', Year='Year', Day='DoY', Hour='Hour')
+    tryCatch({
 
-  #+++ Initalize R5 reference class sEddyProc for processing of eddy data
-  #+++ with all variables needed for processing later
-  EddyProc.C <- REddyProc::sEddyProc$new('EQTP', EddyDataWithPosix.F, c(FLUX,'Rg','Tair','VPD')) #change 'NEE' to other variable names if gapfilling other fluxes (have to match the variable name in the input data file)
-  EddyProc.C$sSetLocationInfo(LatDeg=latitude, LongDeg=longitude, TimeZoneHour=timezone)  #Location of Halsingfors
+      #+++ If not provided, calculate VPD from Tair and rH
+      # EddyData.F <- cbind(EddyData.F,VPD=fCalcVPDfromRHandTair(EddyData.F$rH, EddyData.F$Tair))
 
-  #+++ Fill gaps in variables with MDS gap filling algorithm (without prior ustar filtering)
-  EddyProc.C$sMDSGapFill('Tair', FillAll=FALSE)  	# Gap-filled Tair needed for partitioning and gapfilling
-  EddyProc.C$sMDSGapFill('VPD', FillAll=FALSE)  	# Gap-filled VPD needed for gapfilling
-  EddyProc.C$sMDSGapFill('Rg', FillAll=FALSE)     #Fill only the gaps for the meteo condition, e.g. 'Rg'
-  EddyProc.C$sMDSGapFill(FLUX, FillAll=gapfill_flux)     #Fill all values to estimate flux uncertainties
+      #+++ Add time stamp in POSIX time format
+      EddyDataWithPosix.F <- REddyProc::fConvertTimeToPosix(EddyData.F, 'YDH', Year='Year', Day='DoY', Hour='Hour')
 
-  #+++ Partition NEE into GPP and respiration
-  if(FLUX == "NEE") EddyProc.C$sMRFluxPartition()	# night time partitioning -> Reco, GPP
+      #+++ Initalize R5 reference class sEddyProc for processing of eddy data
+      #+++ with all variables needed for processing later
+      EddyProc.C <- REddyProc::sEddyProc$new('EQTP', EddyDataWithPosix.F, c(FLUX,'Rg','Tair','VPD', 'Ustar')) #change 'NEE' to other variable names if gapfilling other fluxes (have to match the variable name in the input data file)
+      EddyProc.C$sSetLocationInfo(LatDeg=latitude, LongDeg=longitude, TimeZoneHour=timezone)  #Location of Halsingfors
 
-  #+++ Export gap filled and partitioned data to standard data frame
-  FilledEddyData.F <- EddyProc.C$sExportResults()
+      #+++ Fill gaps in variables with MDS gap filling algorithm (without prior ustar filtering)
+      EddyProc.C$sMDSGapFill('Tair', FillAll=FALSE)  	# Gap-filled Tair needed for partitioning and gapfilling
+      EddyProc.C$sMDSGapFill('VPD', FillAll=FALSE)  	# Gap-filled VPD needed for gapfilling
+      EddyProc.C$sMDSGapFill('Rg', FillAll=FALSE)     #Fill only the gaps for the meteo condition, e.g. 'Rg'
+      EddyProc.C$sMDSGapFill(FLUX, FillAll=gapfill_flux)     #Fill all values to estimate flux uncertainties
+      #USTAR THRESHOLD
 
-  #+++ Save results into (tab-delimited) text file in directory
-  CombinedData.F <- cbind(EddyData.F, FilledEddyData.F)
+      EddyProc.C$sEstimateUstarScenarios(
+        nSample = 100L, probs = c(0.05, 0.5, 0.95))
+      # message("ARRIVED HERE")
+      EddyProc.C$sGetEstimatedUstarThresholdDistribution()
 
-  #+++ May rename variables to correspond to Ameriflux
-  colnames(CombinedDataAmeriflux.F <- REddyProc::renameVariablesInDataframe(CombinedData.F, REddyProc::getBGC05ToAmerifluxVariableNameMapping() ))
-  CombinedDataAmeriflux.F$TIMESTAMP_END <- REddyProc::POSIXctToBerkeleyJulianDate( EddyProc.C$sExportData()[[1]] )
-  utils::head(tmp <- REddyProc::BerkeleyJulianDateToPOSIXct( CombinedDataAmeriflux.F$TIMESTAMP_END ))
-  colnames(tmp <- REddyProc::renameVariablesInDataframe(CombinedData.F, REddyProc::getAmerifluxToBGC05VariableNameMapping() ))
-  #opf <- paste0("EQTP_output_sim",toString(i),".txt") #update the output file name
+      EddyProc.C$sMDSGapFillUStarScens(FLUX)
 
+      #+++ Partition NEE into GPP and respiration
+      # if(FLUX == "NEE")  EddyProc.C$sMRFluxPartition()# EddyProc.C$sMRFluxPartitionUStarScens()	# night time partitioning -> Reco, GPP
+
+      if(FLUX == "NEE") EddyProc.C$sMRFluxPartitionUStarScens()
+
+      #+++ Export gap filled and partitioned data to standard data frame
+      FilledEddyData.F <- EddyProc.C$sExportResults()
+
+      #+++ Save results into (tab-delimited) text file in directory
+      CombinedData.F <- cbind(EddyData.F, FilledEddyData.F)
+
+      #+++ May rename variables to correspond to Ameriflux
+      colnames(CombinedDataAmeriflux.F <- REddyProc::renameVariablesInDataframe(CombinedData.F, REddyProc::getBGC05ToAmerifluxVariableNameMapping() ))
+      CombinedDataAmeriflux.F$TIMESTAMP_END <- REddyProc::POSIXctToBerkeleyJulianDate( EddyProc.C$sExportData()[[1]] )
+      utils::head(tmp <- REddyProc::BerkeleyJulianDateToPOSIXct( CombinedDataAmeriflux.F$TIMESTAMP_END ))
+      colnames(tmp <- REddyProc::renameVariablesInDataframe(CombinedData.F, REddyProc::getAmerifluxToBGC05VariableNameMapping() ))
+      #opf <- paste0("EQTP_output_sim",toString(i),".txt") #update the output file name
+
+    }, error=function(e){
+      print("An error occurred:")
+      print(e)
+      CombinedData.F <- EddyData.F
+    })
+    if (year_index == 1){
+      results_df <- CombinedData.F
+    } else {
+      results_df <- merge(results_df, CombinedData.F, all=TRUE)
+    }
+  }
 
   if(is.null(file_name)) file_name <- paste0("For_ReddyProc_", FLUX, "_GF.txt")
-  REddyProc::fWriteDataframeToFile(CombinedData.F, file_name, saving_folder) #update the output file path
+
+  # message(str(CombinedData.F))
+  names(results_df)[which(names(results_df) == "GPP_uStar_f")] <- "GPP_f"
+  names(results_df)[which(names(results_df) == "Reco_uStar")] <- "Reco"
+  # View(CombinedData.F)
+  REddyProc::fWriteDataframeToFile(results_df, file_name, saving_folder) #update the output file path
 
   # #+++ Example plots of filled data to screen or to directory /plots
   #EddyProc.C$sPlotFingerprintY('NEE_f', Year=2021) #gapfilled NEE fingerprint, update the year accordingly
